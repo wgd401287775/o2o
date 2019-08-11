9e434b1a279b2634dd2c780fa182afddcb7ebb49
wgd401287775 <401287775@qq.com>
2019-08-11 21:35:30 +0800

    o2o commit

--- src/main/webapp/resources/js/shop/productedit.js
+++ src/main/webapp/resources/js/shop/productedit.js
@@ -35,6 +35,48 @@ $(function () {
 
     $("#submit").click(function () {
         var product = {};
-
+        product.productName = $("#product-name").val();
+        product.productCategory = {
+            productCategoryId : $("#category").find("option").not(function () {
+                return !this.selected;
+            }).data("id")
+        };
+        product.productDesc = $('#product-desc').val();
+        product.priority = $('#priority').val();
+        product.normalPrice = $('#normal-price').val();
+        product.promotionPrice = $('#promotion-price').val();
+        var formData = new FormData();
+        var smallImg = $("#small-img")[0].files[0];
+        formData.append("productStr", JSON.stringify(product));
+        formData.append("smallImg", smallImg);
+        $(".detail-img").map(function (index, item) {
+            if($(".detail-img")[index].files.length > 0){
+                $('.detail-img')[index].files[0];
+                formData.append("detailImg"+index, $('.detail-img')[index].files[0]);
+            }
+        })
+        var verifyCode = $("#j_captcha").val();
+        if(!verifyCode){
+            $.alert("请输入验证码！");
+            return;
+        }
+        formData.append("verifyCode", verifyCode);
+        $.ajax({
+            url:editUrl,
+            type:"post",
+            data:formData,
+            contentType : false,
+            processData : false,
+            cache : false,
+            success : function (data) {
+                if(data.success){
+                    $.alert('提交成功！');
+                    $('#captcha_img').click();
+                } else {
+                    $.alert('提交失败！');
+                    $('#captcha_img').click();
+                }
+            }
+        })
     });
 })
